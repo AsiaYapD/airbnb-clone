5c45749956b9dee9d9f904b0603fc0b7a01a4766
Asia Yap-Dubois <asiayapd@gmail.com>
2023-12-21 18:56:05 -0500

    Added two pngs to the file

--- App.js
+++ App.js
@@ -18,6 +18,22 @@ export default function App() {
                 title="Life Lessons with Katie Zaferes"
                 price={136}
             />
+             <Card 
+                img="katie-zaferes.png"
+                rating="5.0"
+                reviewCount={6}
+                country="USA"
+                title="Life Lessons with Katie Zaferes"
+                price={136}
+            />
+             <Card 
+                img="katie-zaferes.png"
+                rating="5.0"
+                reviewCount={6}
+                country="USA"
+                title="Life Lessons with Katie Zaferes"
+                price={136}
+            />
         </div>
     )
 }
